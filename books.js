b61a52fde6abfa5a327933e658b7b7742ad11336
Valetina-2608 <valusha2608@gmail.com>
2022-07-25 13:41:10 +0300

    Add books.js

---
+++ books.js
@@ -0,0 +1,36 @@
+
+
+let books = `[
+    {
+        "id": 1,
+        "isRead": false,
+        "title": "Gone with the wind",
+        "description": "The book tells the story of Scarlett O’Hara, the headstrong and willful daughter of the owner of the plantation Tara. "
+    },
+    {
+        "id": 2,
+        "isRead": false,
+        "title": "Harry Poter",
+        "description": "Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry."
+    },
+    {
+        "id": 3,
+        "isRead": false,
+        "title": "The Adventures of Tom Sawyer",
+        "description": "The story describes the adventures of a boy named Tom Sawyer from the little town in the southern United States. The events of the novel take place on the eve of the Civil War in the United States"
+
+    },
+    {
+        "id": 4,
+        "isRead": false,
+        "title": "The Adventures of Sherlock Holmes",
+        "description": "The Adventures of Sherlock Holmes is a collection of twelve short stories by Arthur Conan Doyle, featuring his fictional detective Sherlock Holmes."
+    },
+    {
+        "id": 5,
+        "isRead": false,
+        "title": "Alice in Wonderland",
+        "description": "A young girl named Alice falls through a rabbit hole into a fantasy world of anthropomorphic creatures."
+    }
+    
+]`
